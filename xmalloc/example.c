9181a894ef6e3d07f68b935fec29d822c873b05e
yrmt <youri.mout@gmail.com>
2014-07-11 23:23:53 +0200

    update

--- xmalloc/example.c
+++ xmalloc/example.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-#include <xmalloc.h>
+#include "xmalloc.h"
 #include <stdlib.h>
 
 int main(void) {
     int nb = 100;
     char * string = NULL;
 
-    // no need to check, x* functions
-    // do it for us!
+    // no need to check if destination is NULL, x* functions
+    // do it for us and exit if so.
     xasprintf(&string, "%d", nb);
 
     printf("%s\n", string);
 
+    // test error case
+    char * stemp = xmalloc(0);
+    char * stemp2 = xcalloc(0, sizeof(char));
+
+    free(stemp);
+    free(stemp2);
     free(string);
     string = NULL;
 
